3dd9a4ff08a4ef6b81613934efde15e3ad609031
George Zhao <involute@sina.com>
2022-06-13 03:58:43 +0800

    used new msmarthome account to get token and key

--- custom_components/midea_ac_lan/const.py
+++ custom_components/midea_ac_lan/const.py
@@ -9,5 +9,5 @@ FAN_VERY_LOW = "very low"
 FAN_VERY_HIGH = "very high"
 FAN_FULL_SPEED = "full speed"
 
-OPEN_MIDEA_APP_ACCOUNT = 'midea_is_best@outlook.com'
-OPEN_MIDEA_APP_PASSWORD = 'lovemidea4ever'
+MIDEA_DEFAULT_ACCOUNT = 'b99226f1659e9308@hotmail.com'
+MIDEA_DEFAULT_PASSWORD = '_d0e4e9546a738d_'
